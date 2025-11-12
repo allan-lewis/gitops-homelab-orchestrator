22a3a0967bb822c03e35a28a25d2ec11e55bb24e
allan-lewis <allan.e.lewis@gmail.com>
2025-11-12 07:33:33 -0500

    l2

--- terraform/l2/outputs.tf
+++ terraform/l2/outputs.tf
@@ -1,14 +1,11 @@
-# Collect all VM IDs
+# VMIDs from module outputs
 output "arch_vm_vmids" {
   description = "VMIDs of created Arch VMs"
   value       = { for k, m in module.arch : k => m.vm_id }
 }
 
-# Collect assigned IPv4 addresses (from initialization)
+# IPs from module outputs (not the raw resource tree)
 output "arch_vm_ips" {
   description = "Static IPv4 addresses of created Arch VMs"
-  value = {
-    for k, m in module.arch :
-    k => try(m.initialization[0].ip_config[0].ipv4[0].address, "")
-  }
+  value       = { for k, m in module.arch : k => m.ip_address }
 }
